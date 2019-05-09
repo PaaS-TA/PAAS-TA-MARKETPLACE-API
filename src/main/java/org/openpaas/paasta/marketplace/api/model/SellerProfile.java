d9ff1949d1a6b5f053801d1a76b301743fcb0351
hrjin <hyunlee@bluedigm.com>
2019-05-09 13:23:19 +0900

    판매자 프로필 model 수정 및 등록 로직 수정

--- src/main/java/org/openpaas/paasta/marketplace/api/model/SellerProfile.java
+++ src/main/java/org/openpaas/paasta/marketplace/api/model/SellerProfile.java
@@ -3,14 +3,16 @@ package org.openpaas.paasta.marketplace.api.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
+ * 판매자 프로필 모델
  *
  * @author hrjin
  * @version 1.0
  * @since 2019-05-07
  */
-
 @Data
 @Entity
 public class SellerProfile extends AbstractEntity{
@@ -19,21 +21,22 @@ public class SellerProfile extends AbstractEntity{
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(nullable = false)
+    private String userId;
+
+    @NotNull
     private String sellerName;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private GroupType groupType;
+    @NotNull
+    private String businessType;
+
+    @Transient
+    private List<CustomCode> businessTypeList;
 
     private String managerName;
 
-    @Column(nullable = false)
+    @NotNull
     private String email;
 
     private String homepageUrl;
 
-    public enum GroupType {
-        publicEnterprise, enterprise, individual ,etc
-    }
 }
