bde51201f0b3f066303b73b97c4120b366ab857f
peter <jaehaklee@bluedigm.com>
2019-05-15 17:19:33 +0900

    test 코드 수정

--- src/test/java/org/openpaas/paasta/marketplace/api/ApplicationTests.java
+++ src/test/java/org/openpaas/paasta/marketplace/api/ApplicationTests.java
@@ -2,14 +2,20 @@ package org.openpaas.paasta.marketplace.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest
-@WebMvcTest(Application.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class ApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+	
 	@Test
 	public void contextLoads() {
 	}
