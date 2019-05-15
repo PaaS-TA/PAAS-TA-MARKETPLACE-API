14f2d57ea7548bd0694e5772d73ad72bacbeb853
peter <jaehaklee@bluedigm.com>
2019-05-15 14:14:21 +0900

    테스트 방식 변경

--- src/test/java/org/openpaas/paasta/marketplace/api/ApplicationTests.java
+++ src/test/java/org/openpaas/paasta/marketplace/api/ApplicationTests.java
@@ -2,11 +2,12 @@ package org.openpaas.paasta.marketplace.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest
+@WebMvcTest(Application.class)
 public class ApplicationTests {
 
 	@Test
