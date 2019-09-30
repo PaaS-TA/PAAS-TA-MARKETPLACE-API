a619860f07c9cc1597b285a32179bf57b829d09e
geyoo <geyoo@bluedigm.com>
2019-09-30 16:21:47 +0900

    판매자 프로필 수정 및 저장 API 추가, 웹화면 추가

--- src/main/java/org/openpaas/paasta/marketplace/api/controller/AdminSellerProfileController.java
+++ src/main/java/org/openpaas/paasta/marketplace/api/controller/AdminSellerProfileController.java
@@ -5,10 +5,14 @@ import javax.validation.constraints.NotNull;
 import org.openpaas.paasta.marketplace.api.domain.Profile;
 import org.openpaas.paasta.marketplace.api.domain.ProfileSpecification;
 import org.openpaas.paasta.marketplace.api.service.ProfileService;
+import org.openpaas.paasta.marketplace.api.util.SecurityUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +34,16 @@ public class AdminSellerProfileController {
     public Profile get(@NotNull @PathVariable String id) {
         return profileService.get(id);
     }
-
+    
+ 
+    @PutMapping("/{id}")
+    public Profile update(@PathVariable @NotNull String id, @NotNull @Validated @RequestBody Profile profile) {
+        return profileService.update(profile);
+    }
 }
+
+
+
+
+
+
