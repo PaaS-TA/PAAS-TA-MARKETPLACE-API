ad3825015aa8345739076d379aaeba1cbbf42512
woozoo <woozoo@10.0.75.1>
2019-10-10 11:34:05 +0900

    AdonisTrack 버전 업데이트

--- src/main/java/org/openpaas/paasta/marketplace/api/Application.java
+++ src/main/java/org/openpaas/paasta/marketplace/api/Application.java
@@ -2,7 +2,11 @@ package org.openpaas.paasta.marketplace.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.woozooha.adonistrack.filter.AdonistrackFilter;
 
 @SpringBootApplication
 @EnableScheduling
@@ -10,6 +14,15 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<AdonistrackFilter> profileFilter() {
+        FilterRegistrationBean<AdonistrackFilter> registrationBean = new FilterRegistrationBean<AdonistrackFilter>();
+        registrationBean.setFilter(new AdonistrackFilter());
+        registrationBean.addUrlPatterns("/*");
+
+        return registrationBean;
     }
 
 }
--- src/main/java/org/openpaas/paasta/marketplace/api/config/AdonisTrackAspect.java
+++ src/main/java/org/openpaas/paasta/marketplace/api/config/AdonisTrackAspect.java
@@ -4,15 +4,11 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
-import com.woozooha.adonistrack.ProfileAspect;
+import com.woozooha.adonistrack.aspect.ProfileAspect;
 
 @Component
 @Aspect
 public class AdonisTrackAspect extends ProfileAspect {
-
-    @Pointcut("execution(* org.openpaas.paasta.marketplace.api.controller.*Controller.*(..))")
-    protected void profilePointcut() {
-    }
 
     @Pointcut("execution(* *(..)) && (within(org.openpaas.paasta.marketplace.api..*) || within(org.openpaas.paasta.marketplace.api..*+)) && !within(org.openpaas.paasta.marketplace.api.config..*)")
     protected void executionPointcut() {
