45ec547035fecf98cb7f7b178d39d71e69d690a5
sjchoi <sjchoi@bluedigm.com>
2019-10-10 17:00:50 +0900

    사용자별 사용연월 데이터 수정(사용 종료일이 없을 때 현재까지에 대한 그래프 추이)

--- src/main/java/org/openpaas/paasta/marketplace/api/repository/StatsRepository.java
+++ src/main/java/org/openpaas/paasta/marketplace/api/repository/StatsRepository.java
@@ -95,10 +95,15 @@ public interface StatsRepository extends JpaRepository<Stats<Long, Long>, Long> 
             + "AND ((i.usageStartDate <= :start AND (i.usageEndDate IS NULL OR i.usageEndDate < :end)) OR (i.usageStartDate >= :start AND i.usageStartDate < :end))")
     long countOfInstsUsing(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
+//    @Query("SELECT COUNT(*) FROM Instance i WHERE i.createdBy = :createdBy "
+//            + "AND i.usageStartDate >= :start AND i.usageStartDate < :end")
+//    long countOfInstsUserApproval(@Param("createdBy") String createdBy, @Param("start") LocalDateTime start,
+//            @Param("end") LocalDateTime end);
+
     @Query("SELECT COUNT(*) FROM Instance i WHERE i.createdBy = :createdBy "
-            + "AND i.usageStartDate >= :start AND i.usageStartDate < :end")
-    long countOfInstsUserApproval(@Param("createdBy") String createdBy, @Param("start") LocalDateTime start,
-            @Param("end") LocalDateTime end);
+            + "AND i.usageStartDate IS NOT NULL "
+            + "AND ((i.usageStartDate <= :start AND (ifnull(i.usageEndDate, now()) >= :start AND ifnull(i.usageEndDate, now()) < :end)) OR (i.usageStartDate >= :start AND i.usageStartDate < :end)) ")
+    long countOfInstsUserApproval(@Param("createdBy") String createdBy, @Param("start") LocalDateTime start,  @Param("end") LocalDateTime end);
 
     @Query("SELECT COUNT(*) FROM Instance i WHERE i.createdBy = :createdBy " + "AND i.usageStartDate IS NOT NULL "
             + "AND ((i.usageStartDate <= :start AND (i.usageEndDate IS NULL OR i.usageEndDate < :end)) OR (i.usageStartDate >= :start AND i.usageStartDate < :end))")
