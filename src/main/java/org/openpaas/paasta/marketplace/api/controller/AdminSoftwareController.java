536acc1699f0222d9f8063e3384313ac3ff1f338
sjchoi <sjchoi@bluedigm.com>
2019-10-24 22:09:45 +0900

    상품 상품/수정 판매금액 이력 조회

--- src/main/java/org/openpaas/paasta/marketplace/api/controller/AdminSoftwareController.java
+++ src/main/java/org/openpaas/paasta/marketplace/api/controller/AdminSoftwareController.java
@@ -2,11 +2,10 @@ package org.openpaas.paasta.marketplace.api.controller;
 
 import javax.validation.constraints.NotNull;
 
-import org.openpaas.paasta.marketplace.api.domain.Software;
-import org.openpaas.paasta.marketplace.api.domain.SoftwareHistory;
-import org.openpaas.paasta.marketplace.api.domain.SoftwareHistorySpecification;
-import org.openpaas.paasta.marketplace.api.domain.SoftwareSpecification;
+import org.openpaas.paasta.marketplace.api.domain.*;
+import org.openpaas.paasta.marketplace.api.service.SoftwarePlanService;
 import org.openpaas.paasta.marketplace.api.service.SoftwareService;
+import org.openpaas.paasta.marketplace.api.util.SecurityUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -31,6 +30,9 @@ public class AdminSoftwareController {
 
     private final SoftwareService softwareService;
 
+    private final SoftwarePlanService softwarePlanService;
+
+    //[1]Software
     @GetMapping("/page")
     public Page<Software> getPage(SoftwareSpecification spec, Pageable pageable) {
         return softwareService.getPage(spec, pageable);
@@ -45,11 +47,12 @@ public class AdminSoftwareController {
     public Software update(@PathVariable @NotNull Long id,
             @NotNull @Validated(Software.UpdateMetadata.class) @RequestBody Software software, BindingResult bindingResult)
             throws BindException {
-
-//        Software sameName = softwareService.getByName(software.getName());
-//        if (sameName != null && id != sameName.getId()) {
-//            bindingResult.rejectValue("name", "Unique");
-//        }
+        /*
+        Software sameName = softwareService.getByName(software.getName());
+        if (sameName != null && id != sameName.getId()) {
+         bindingResult.rejectValue("name", "Unique");
+         }
+        */
 
         if (bindingResult.hasErrors()) {
             throw new BindException(bindingResult);
@@ -60,12 +63,29 @@ public class AdminSoftwareController {
         return softwareService.updateMetadata(software);
     }
 
+    //[2]SoftwareHistory
     @GetMapping("/{id}/histories")
     public List<SoftwareHistory> getHistoryList(@NotNull @PathVariable Long id, Sort sort) {
         SoftwareHistorySpecification spec = new SoftwareHistorySpecification();
         spec.setSoftwareId(id);
 
         return softwareService.getHistoryList(spec, sort);
+    }
+
+
+    //[3]SoftwarePlan
+    @GetMapping("/plan/{id}")
+    public SoftwarePlan getPlan(@NotNull @PathVariable Long id) {
+        return softwarePlanService.get(id);
+    }
+
+
+    @GetMapping("/plan/{id}/histories")
+    public List<SoftwarePlanHistory> getPlanHistoryList(@NotNull @PathVariable Long id, Sort sort) {
+        SoftwarePlanHistorySpecification spec = new SoftwarePlanHistorySpecification();
+        spec.setSoftwareId(id);
+
+        return softwarePlanService.getHistoryList(spec, sort);
     }
 
 }
