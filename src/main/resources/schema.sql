fb8446b9657ee99bc99d175377a862d98c7193ba
비디 심정범 <jbshim@bluedigm.com>
2019-11-19 12:42:38 +0900

    comment 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `calendar` (
-  `dt` char(8) CHARACTER SET latin1 DEFAULT NULL,
-  `ym` char(6) CHARACTER SET latin1 DEFAULT NULL,
-  `days` int(11) DEFAULT NULL,
-  `last_modified_date` datetime DEFAULT NULL,
+  `dt` char(8) CHARACTER SET latin1 DEFAULT NULL COMMENT '날짜(연월일)',
+  `ym` char(6) CHARACTER SET latin1 DEFAULT NULL COMMENT '연월',
+  `days` int(11) DEFAULT NULL COMMENT '월별 일수',
+  `last_modified_date` datetime DEFAULT NULL COMMENT '최종 수정일',
   PRIMARY KEY (`dt`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='통계용 날짜 테이블'
 ;
